256c21fb51a11cd0e16526e2bfbb0eeb46239981
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-06-01 00:21:29 -0500

    terminada funcionalidad con jQuery, aun sigue con estilos básicos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,3 @@
-// window.addEventListener("load", function() {
-//
-//   var toDoForm = document.getElementById("toDoForm");
-//   toDoForm.addEventListener("submit", function (e) {
-//     e.preventDefault();
-//
-//     // obtencion de datos
-//     var contenedor = document.getElementById("posts");
-//     var mensajeContenedor = document.getElementById("message");
-//     var mensaje = mensajeContenedor.value;
-//
-//     // creacion de elementos
-//     var postContenedor = document.createElement("article");
-//     var post = document.createElement("p");
-//
-//     // personalizacion de elementos creados
-//     postContenedor.className = "jumbotron";
-// 		post.textContent = mensaje;
-//
-//     // agregar al DOM
-//     postContenedor.appendChild(post);
-//     contenedor.appendChild(postContenedor);
-//
-//     mensajeContenedor.value = "";
-//   });
-// });
-//INICIA jQuery
 var funcionAEjecutar = function () {
   // elemento traido a partir de id
   var $toDoForm = $("#toDoForm");
